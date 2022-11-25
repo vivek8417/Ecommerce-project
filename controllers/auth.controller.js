18dafc1e548e01c6c507cbd54f0093928e3eda1b
vivek8417 <viveksgn012@gmail.com>
2022-11-25 21:06:40 +0530

    signin created

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,37 +4,74 @@ const config = require("../Config/auth.config")
 const User = db.user
 const Role = db.role
 
-const Op= db.Sequelize.Op;
+const Op = db.Sequelize.Op;
 
 var jwt = require("jsonwebtoken")
 
 var bcrypt = require("bcryptjs")
 
-exports.signup = (req, res)=>{
+exports.signup = (req, res) => {
     User.create({
-        username : req.body.username,
-        email : req.body.email,
-        password : bcrypt.hashSync(req.body.password,8)
+        username: req.body.username,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 8)
     })
-    .then(user => {
-        if(req.body.roles){
-            Role.findAll({
-                where:{
-                    name:{
-                        [Op.or]:req.body.roles
+        .then(user => {
+            if (req.body.roles) {
+                Role.findAll({
+                    where: {
+                        name: {
+                            [Op.or]: req.body.roles
+                        }
                     }
-                }
-            })
-            .then(roles =>{
-                //populate user_roles table
-                user.setRoles(roles)
-                .then(() =>{
-                    res.status(200).send({message: "User registered successfully"})
                 })
-            })
+                    .then(roles => {
+                        //populate user_roles table
+                        user.setRoles(roles)
+                            .then(() => {
+                                res.status(200).send({ message: "User registered successfully" })
+                            })
+                    })
+            }
+        })
+        .catch(err => {
+            res.status(500).send("Unable to add the user"+ err.message)
+        })
+}
+
+exports.signin = (req, res) => {
+
+    User.findOne({
+        where: {
+            username: req.body.username
         }
     })
-    .catch(err =>{
-        res.status(500).send("Unable to add the user"+err.message)
-    })
+        .then(user => {
+            if (!user) {
+                res.status(404).send({ message: "User not found" })
+            }
+
+            //check if the password is valid or not
+            var passwordIsValid = bcrypt.compareSync(req.body.password, user.password)
+
+            if (!passwordIsValid) {
+                return res.status(401).send({
+                    message: ' Invalid Password'
+                })
+            }
+
+            //if both username and password are valid, created the token
+
+            var token = jwt.sign({ id: user.id }, config.secret, { expiresIn: 86400 })  // 24 hrs
+
+            res.status(200).send({
+                username: user.username,
+                accestoken: token
+            })
+                
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message
+            })
+        })
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,6 @@ const controller= require("../controllers/auth.controller.js");
 module.exports=function(app){
 
     app.post("/ecomm/api/v1/auth/signup", controller.signup)
-   // app.post("/ecomm/api/v1/auth/signin", controller.signin)
+    app.post("/ecomm/api/v1/auth/signin", controller.signin)
 
 }
