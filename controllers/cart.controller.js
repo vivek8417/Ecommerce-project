7d38d6bc0fd6cf0eabd0d6c26ea7248f0cf4f7ea
vivek8417 <viveksgn012@gmail.com>
2022-11-29 21:07:38 +0530

    Added cart update api

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,4 +1,5 @@
 
+
 const db = require("../models");
 const Product = db.product;
 const Cart = db.cart
@@ -17,5 +18,52 @@ exports.create= (req, res)=>{
         res.status(500).send({
             message : "Some internal server issue happned"
         })
+    })
+}
+
+exports.update = (req, res) =>{
+
+    let cartId = req.params.id;
+    let oldCost =0;  // cost of the items already existing in the cart
+
+    Cart.findByPk(cartId)
+    .then(cart =>{
+        oldCost = cart.cost;
+
+        Product.findAll({
+            where :{
+                id : req.body.productIds
+            }
+        })
+        .then(items => {
+            cart.setProducts(items) // add those products to the cart product table
+            .then(()=>{
+
+                var NewCost= 0;
+                cart.getProducts()
+                .then(products =>{
+
+                    //add the cost ot all the items in the cart
+
+                    for(i=0; i<products.length;i++){
+                        NewCost= NewCost + products[i].cost
+                    }
+                    // updating the final cost in the cart table.
+                    Cart.update({cost : oldCost + NewCost}, {where : {id : cartId}})
+
+                    res.status(200).send({
+                        message : "Successfully added items to the cart"
+                    })
+                })
+            })
+        })
+        .catch(err =>{
+            res.status(500).send("Some internal error happned while fetching the product details")
+
+        })
+    })
+    .catch(err=>{
+            res.status(500).send("Some internal error happned while fetching the carts details")
+           
     })
 }
